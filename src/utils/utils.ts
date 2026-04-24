07ab1b8db36a9da6243e0ebe457a47d59f069187
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2026-04-24 18:01:00 +0800

    refactor: add utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,11 @@ export function log<T>(item: T): T {
   console.dir(item)
   return item
 }
+
+export function internalError(): never {
+  throw new Error('internal error')
+}
+
+export function unreachable(): never {
+  throw new Error('Unreachable branch')
+}
