368cacaba2b935cdf6a35d64f69c2531e46a04b0
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2026-04-24 15:33:12 +0800

    feat: add constructors

--- src/frontend/parser/super-combinator.ts
+++ src/frontend/parser/super-combinator.ts
@@ -5,3 +5,12 @@ export interface Supercombinator {
   args: string[]
   body: Expr
 }
+
+export const Supercombinator = {
+  from(name: string, args: string[], body: Expr): Supercombinator {
+    return { name, args, body }
+  },
+  from_name(name: string): Supercombinator {
+    return { name, args: [], body: Expr.error() }
+  },
+}
