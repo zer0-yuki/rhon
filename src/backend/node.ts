1e883d49623bb6318f051b594d85476981ea55c3
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2026-05-12 17:39:31 +0800

    chore: rename value of number & string node to `value`

--- src/backend/node.ts
+++ src/backend/node.ts
@@ -1,18 +1,18 @@
 import { ScDef } from './sc-def.js'
 
 export type Node =
-  | { kind: 'number'; n: number }
-  | { kind: 'string'; str: string }
+  | { kind: 'number'; value: number }
+  | { kind: 'string'; value: string }
   | { kind: 'app'; fnAddr: number; argAddr: number }
   | { kind: 'global'; def: ScDef }
   | { kind: 'ind'; addr: number }
 
 export const Node = {
   number(n: number): Node {
-    return { kind: 'number', n }
+    return { kind: 'number', value: n }
   },
   string(str: string): Node {
-    return { kind: 'string', str }
+    return { kind: 'string', value: str }
   },
   app(fnAddr: number, argAddr: number): Node {
     return { kind: 'app', fnAddr, argAddr }
