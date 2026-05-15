ba3a9bb970196b10f9953660148eee491f3729e4
0yuki <147019313+Aoyukiii@users.noreply.github.com>
2026-05-15 20:35:10 +0800

    feat(lexer): add number test

--- src/frontend/lexer/index.test.ts
+++ src/frontend/lexer/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, beforeEach } from 'node:test'
 import assert from 'assert'
 import { Lexer } from './index.js'
-import { charToKind, Token } from './token.js'
+import { charToKind, NumberToken, Token } from './token.js'
 
 describe('Lexer', () => {
   describe('single operators', () => {
@@ -11,5 +11,21 @@ describe('Lexer', () => {
         assert.deepStrictEqual(tokens, [Token.symbol(kind)])
       })
     }
+  })
+
+  describe('number literals', () => {
+    const testCases: [string, NumberToken][] = [
+      ['42', Token.number('42')],
+      ['+42', Token.number('+42')],
+      ['-42', Token.number('-42')],
+      [' +42', Token.number('+42')],
+      [' -42', Token.number('-42')],
+    ]
+    testCases.forEach(([input, expected]) => {
+      it(`lexes '${input}' as number token`, () => {
+        const tokens = [...new Lexer(input)]
+        assert.deepStrictEqual(tokens, [expected])
+      })
+    })
   })
 })
